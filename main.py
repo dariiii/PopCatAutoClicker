acac0b4ecaf1ff96d200eed703d7f311e7d603f0
goldfirework <57460083+goldfirework@users.noreply.github.com>
2021-04-09 16:09:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,4 @@ while(int(clicks) > clicks2):
     #print("click: " + str(clicks2 * 500))
     elem.send_keys("""afCDDD0zoMRGJmeOuiB6v2worCXYDRYcR9y91MqRiEpL5rC3tiGguhwmMTDxLp065RH8Re9aXvvjxD4xaLS7eC8lV1cwmLYTF6rhsKMfNV50GFvAUppYkMZsz8PqvZkPxbiatJgbcnCWOeJZnaDfeeVJFp2qDnPBme0jEVmBuCQaQp2RyLEEYElNLj1PQ68HzejGdztvYpNchsKjXzxAbHXVPbU6dYxE9ZZ8llzvWlumxS22fHKJ4aZzR44QZxgBCVfcrb4i7Z21qsSeeBgPC9QDihaipvWHqPTiQGNkKzrxM8ANeSJGFu2Qw9xi9AnDCo6an47ddU6bBr2Rgl2F3rwUK2ySOPuk4nvJraQROOMekfHrjfoAG01Sl5H1oPNH8jlZaa8hG6S1XJNRxCFeboq3JJucuWaMRjX9wvji5jsQSqymOBgEqmjFfSzTjI7zU7Vv95DFpmdN1TLtWP3kzYh0OEB6JDxLggC9Vrp9RXV7lu5PPPwQ""")
 
-<<<<<<< HEAD
 #print(clicks + " sent")
-=======
-#print(clicks + " sent")
->>>>>>> c4ca055c1994bf0d23aa79f7d365693c53a81bcc
